e1b899c14641cf3a9027a9487431030fbdb188d8
Leroy <leroybaeyens@gmail.com>
2019-03-17 11:27:18 +0100

    Changed before install script to download php-cs-fixer with composer

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,7 +2,4 @@
 
 set -ev
 
-mkdir --parents "${HOME}/bin"
-
-wget "http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar" --output-document="${HOME}/bin/php-cs-fixer"
-chmod u+x "${HOME}/bin/php-cs-fixer"
+composer global require friendsofphp/php-cs-fixer
