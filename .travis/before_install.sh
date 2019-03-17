f1a7775cec1e4614de38262e63e8cbe337cf05b2
Leroy <leroybaeyens@gmail.com>
2019-03-17 12:04:16 +0100

    Add composer bin to PATH

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,4 +4,4 @@ set -ev
 
 composer global require friendsofphp/php-cs-fixer
 
-export PATH="${PATH}:${HOME}/.composer/vendor/bin"
+export PATH="$PATH:$HOME/.config/composer/vendor/bin:$HOME/.composer/vendor/bin"
